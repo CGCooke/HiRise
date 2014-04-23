0ee5c46c984b1eb9d5a0fdd34f7b692e618cdd5e
CGCooke <Cameron.G.Cooke@mail.com>
2014-04-24 01:39:46 +1000

    Improved shifting algorithm
    Basically, instead of just shifting pixels right, move both left and
    right, in equal proportions

--- Mars/ImageShifting.py
+++ Mars/ImageShifting.py
@@ -12,18 +12,20 @@ import numpy as np
 import gdal
 
 def shiftImage(scenePath = 'SHRUNK.TIF'):
+	depthShiftingFactor =100
 	ds = gdal.Open(scenePath, gdal.GA_ReadOnly)
 	DEM=(ds.GetRasterBand(1).ReadAsArray())
-	inputImg = np.asarray(Image.open('OUT.TIF'))
+	inputImg = np.asarray(Image.open('combined.TIF'))
 	
 	outputImg = np.zeros((inputImg.shape[0],inputImg.shape[1]*2,3),dtype =np.uint8)
-	
+
 	''' There are 2 ways to do this, loop through every output pixel, or loop through every input pixel '''
 	for i in range(0,inputImg.shape[1]):
 		for j in range(0,inputImg.shape[0]):
-			outputImg[j,i+inputImg.shape[1],:] = inputImg[j,i,:]
 			try:
-				outputImg[j,i,:] = inputImg[j,i+DEM[j,i]/100,:]   
+				outputImg[j,i,:] = inputImg[j,i+DEM[j,i]/depthShiftingFactor,:]   
+				outputImg[j,i+inputImg.shape[1],:] = inputImg[j,i-DEM[j,i]/depthShiftingFactor,:]
+			
 			except:
 				IndexError
 	img = Image.fromarray(outputImg)
